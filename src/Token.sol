5eb3b2ad813ebca19c10e2c631b22ed4dc70323b
dev | HOME <gokhan.plt@gmail.com>
2022-07-14 00:26:47 +0300

    ADDED - .dir-locals example file

--- src/Token.sol
+++ src/Token.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-// import {ERC20} from "solmate/src/tokens/ERC20.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import { ERC20 } from "solmate/src/tokens/ERC20.sol";
+// import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20("Token", "TOK") {
+contract Token is ERC20("Token", "TOK", 18) {
 
     string public name_str;
     uint256 num;
     constructor() {
         name_str = "check str";
-        // num = -1;
     }
 }
