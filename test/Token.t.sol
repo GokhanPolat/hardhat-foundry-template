7bf264790464a431da53236ec38cb3c395137e62
dev | HOME <gokhan.plt@gmail.com>
2022-07-14 00:44:30 +0300

    ADDED - Makefile

--- test/Token.t.sol
+++ test/Token.t.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/src/Test.sol";
+import {console} from "forge-std/src/console.sol";
+import {console2} from "forge-std/src/console2.sol";
 
 import "../src/Token.sol";
 
@@ -17,6 +19,8 @@ contract TokenTest is Test {
     }
 
     function testName_str() public {
+        console2.log("When transferring tokens");
+        console2.log(t.name_str());
         assertEq(t.name_str(), "check str");
     }
 }
